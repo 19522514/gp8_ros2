5730a436ce3a01bee3d0fa20b79f9eeb51c2ace9
19522514 <cchew3103@gmail.com>
2026-07-12 14:09:26 -0700

    fixed launch args

--- gp8_gazebo/launch/gp8.gazebo.launch.py
+++ gp8_gazebo/launch/gp8.gazebo.launch.py
@@ -42,7 +42,7 @@ def generate_launch_description():
 
     default_robot_name = 'gp8'
     gazebo_models_path = 'models'
-    default_world_file = 'pick_and_place_demo.world'
+    default_world_file = 'empty.world'
     gazebo_worlds_path = 'worlds'
 
     ros_gz_bridge_config_file_path = 'config/ros_gz_bridge.yaml'
